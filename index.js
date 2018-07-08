746b79eb74c3cf6a00ae116b7f84d0bbc8477496
Stéphane G <stephane.gronowski@gmail.com>
2018-07-08 10:09:02 +0200

    Creation de la main window

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const electron = require('electron');
 
-// Main process
-const {app} = electron;
+// Main process, main window
+const {app, BrowserWindow} = electron;
 
 app.on('ready', () => {
+    const mainWindow = new BrowserWindow({});
+    
+    mainWindow.loadURL(`file://${__dirname}/index.html`);
+
     console.log('App lock and loaded !')
-})
+});
