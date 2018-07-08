517cd7563d83ad376ba5d34eddc69b0cc8afd42a
Stéphane G <stephane.gronowski@gmail.com>
2018-07-08 11:36:26 +0200

    Utilisation de FFmpeg pour récupérer les infos d'une vidéo

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const electron = require('electron');
+const ffmepg = require('fluent-ffmpeg');
 
-// Main process, main window
-const {app, BrowserWindow} = electron;
+// Main process, main window, ipc main
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
     const mainWindow = new BrowserWindow({});
-    
+
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-    console.log('App lock and loaded !')
+    console.log('App lock and loaded !');
+});
+
+ipcMain.on('video:submit', (event, path) => {
+    console.log('video:submit received, path ' + path);
+
+    ffmepg.ffprobe(path, (err, metadata) => {
+        console.log('Video duration : ' + metadata.format.duration);
+    });
 });
